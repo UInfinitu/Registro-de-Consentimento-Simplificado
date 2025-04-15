04cc911ad9b6c51899c098851411af267655185b
Hugo Facchini <127160695+UInfinitu@users.noreply.github.com>
2025-04-15 15:14:42 -0300

    Diagramação
    Criados diagramas de Caso de Uso (sem descrição de caso de uso), Classes e Sequência (apenas do processo do usuário registrar um novo consentimento). Criado o MER (Modelo Entidade-Relacionamento) do banco de dados, junto a isso, o código para sua implementação também foi feito.

---
+++ Banco - Registro de Consentimento Simplificado.sql
@@ -0,0 +1,42 @@
+CREATE SCHEMA IF NOT EXISTS 'rcs' DEFAULT CHARACTER SET utf8 ;
+USE 'rcs' ;
+
+-- -----------------------------------------------------
+-- Tabela para usuários
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'rcs'.'usuario' (
+  'idUsuario' INT NOT NULL,
+  'nomeUsuario' VARCHAR(45) NOT NULL,
+  'loginUsuario' VARCHAR(45) NOT NULL,
+  'senhaUsuario' VARCHAR(45) NOT NULL,
+  PRIMARY KEY ('idUsuario'));
+
+-- -----------------------------------------------------
+-- Tabela de finalidades
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'rcs'.'finalidade' (
+  'idFinalidade' INT NOT NULL,
+  'nomeFinalidade' VARCHAR(45) NOT NULL,
+  'descFinalidade' VARCHAR(100) NOT NULL,
+  PRIMARY KEY ('idFinalidade'));
+
+-- -----------------------------------------------------
+-- Tabela de consentimentos que é onde relaciona os usuários e as finalidades
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS 'rcs'.'consentimentos' (
+  'usuario_idUsuario' INT NOT NULL,
+  'finalidade_idFinalidade' INT NOT NULL,
+  'estado' TINYINT NOT NULL, -- Se a coleta de dados está consentida ou não (0 ou 1)
+  PRIMARY KEY ('usuario_idUsuario', 'finalidade_idFinalidade'),
+  INDEX 'fk_usuario_has_finalidade_finalidade1_idx' ('finalidade_idFinalidade' ASC) VISIBLE,
+  INDEX 'fk_usuario_has_finalidade_usuario_idx' ('usuario_idUsuario' ASC) VISIBLE,
+  CONSTRAINT 'fk_usuario_has_finalidade_usuario'
+    FOREIGN KEY ('usuario_idUsuario')
+    REFERENCES 'rcs'.'usuario' ('idUsuario')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT 'fk_usuario_has_finalidade_finalidade1'
+    FOREIGN KEY ('finalidade_idFinalidade')
+    REFERENCES 'rcs'.'finalidade' ('idFinalidade')
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
